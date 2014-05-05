a7f665c21832c3af127f569c8932593d798a7364
Romain Hardy <romain.hardy17@gmail.com>
2014-05-05 13:35:05 +0100

    Cleanup: remove useless files

--- client/assets/stylesheets/less.watchr
+++
@@ -1,6 +0,0 @@
-def action
-    system("echo less file modified")
-    system("lessc less/main.less > main.css")
-end
-
-watch( 'less/*') { action }
--- client/bin/less.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-watchr -e 'watch("../assets/stylesheets/less/(.*).less") { |f| system("echo lol && lessc ../assets/stylesheets/less/main.less > ../assets/stylesheets/main.css")}'
