e0ca3e75c87748b30bc4773244bb0ae2840bcc7c
Romain Hardy <romain.hardy17@gmail.com>
2014-05-13 20:22:38 +0100

    add Deployment task wih gulp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,6 +3,9 @@ var path = require('path');
 var less = require('gulp-less');
 var handlebars = require('gulp-handlebars');
 var defineModule = require('gulp-define-module');
+var requirejs = require('requirejs');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 // Compile less files
 gulp.task('less', function () {
@@ -27,4 +30,22 @@ gulp.task('default', ['less', 'templates'], function() {
 
   // Watch handlebars templates files
   gulp.watch('./app/templates/**/*.handlebars', ['templates']);
+});
+
+gulp.task('build', ['less', 'templates'], function() {
+  requirejs.optimize({
+    dir: 'build',
+    baseUrl: 'app/',
+    removeCombined: true,
+    mainConfigFile: './app/config.js',
+    modules: [{ "name": "config" }],
+    optimize: 'none'
+    });
+
+    // Concat result in one file
+     gulp.src(['bower_components/requirejs/require.js','build/config.js'])
+      .pipe(uglify())
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('./dist/'))
+
 });
